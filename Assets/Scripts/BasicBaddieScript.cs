1b3c0ed07e47ddd8b0180202fe7e1f70643bc278
arhands <andrewrhands@gmail.com>
2021-06-19 18:35:27 -0500

    Working on basic AI
    Added AI character that moves randomly.

--- Assets/Scripts/BasicBaddieScript.cs
+++ Assets/Scripts/BasicBaddieScript.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class BasicBaddieScript : MonoBehaviour
 {
     // use this to get parent obj.
-    public Sprite Parent { get; set; }
+    public Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 dF = Random.insideUnitCircle;
+        dF.Scale(new Vector2(4f,4f));
+        body.AddForce(dF);
     }
 }
