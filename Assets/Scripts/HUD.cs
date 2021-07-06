b0d2bad94cc85bf73e3a30b1a736162f479de15e
CJ13-projects <84744579+CJ13-projects@users.noreply.github.com>
2021-07-06 16:06:18 -0500

    HUD
    HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Health health;
+
+    // Update is called once per frame
+    public void setData(MainCharacter mainCharacter)
+    {
+        health.setHP((float)mainCharacter.health);    
+    }
+}
