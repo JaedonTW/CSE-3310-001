8ac60428ca4b4118526ec23aa1caf229b90e1a17
JaedonTW <76446272+JaedonTW@users.noreply.github.com>
2021-07-19 17:25:43 -0500

    Added Puzzle Door Script
    The Puzzle Door Class inherits from the Door Class. This is needed as not every door will function the same in each level.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,23 +5,9 @@ using TMPro;
 
 public class Door : MonoBehaviour
 {
-    /*
-        door_Order is a SortedDictionary that we will use to
-        determine the order in which Harris must enter the doors
-        to finish level 2. 
-    */
-    SortedDictionary<string, int> door_Order = new SortedDictionary<string, int>()
-    {
-        {"Viper",0},
-        {"Reed",1},
-        {"Water Ripple",2},
-        {"Vulture",3},
-        {"Lion", 4}
-    };
-    
     public Animator animator;
     public TextMeshProUGUI textMeshPro;
-    private GameManager manager;
+    protected GameManager manager;
 
     /*
         The following boolean will be
@@ -29,64 +15,37 @@ public class Door : MonoBehaviour
         either opened or closed, which will trigger
         the proper animation to be played.
     */
-    private bool isOpen;
-
+    protected bool isOpen;
     /*
         Change_Door_State() will be used to set the
         correct boolean variables regarding the state
         of the door.
     */
-    void Change_Door_State() 
+    protected void Change_Door_State()
     {
-        if (isOpen == false) 
+        if (isOpen == false)
         {
             isOpen = true;
         }
 
-        else 
+        else
         {
             isOpen = false;
         }
     }
-
-    private void OnMouseDown()
-    {
-        Change_Door_State();
-
-        /*
-            The gameObject's tag is the name of
-            the door defined in the inspector.
-        */
-        manager.Track_Door_Order(door_Order,gameObject.tag);
-
-        if(isOpen == true) 
-        {
-            //textMeshPro.GetComponent<TextMeshProUGUI>().text = gameObject.tag + " is opened!";
-            Debug.Log("OPENING!\n");
-        }
-
-        else
-        {
-            Debug.Log("CLOSING\n");
-            //textMeshPro.GetComponent<TextMeshProUGUI>().text = gameObject.tag + " is closed!";
-        }
-    }
-
     
-    void Start()
+    private void Start()
     {
+        // Find reference to the GameManager object.
         manager = FindObjectOfType<GameManager>();
-        /*
-            The door will start off
-            closed.
-        */
-        isOpen = false;
 
+        // The door will begin closed.
+        isOpen = false;
     }
 
-    
-    void Update()
+    private void Update()
     {
-        animator.SetBool("isOpen",isOpen);
+        // Check for animation update every frame.
+        animator.SetBool("isOpen", isOpen);
     }
 }
---
+++ Assets/Scripts/Puzzle_Door.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Puzzle_Door : Door
+{
+    /*
+        door_Order is a SortedDictionary that we will use to
+        determine the order in which Harris must enter the doors
+        to finish level 2. 
+    */
+    SortedDictionary<string, int> door_Order = new SortedDictionary<string, int>()
+    {
+        {"Viper",0},
+        {"Reed",1},
+        {"Water Ripple",2},
+        {"Vulture",3},
+        {"Lion", 4}
+    };
+
+    private void OnMouseDown()
+    {
+        Change_Door_State();
+
+        /*
+            The gameObject's tag is the name of
+            the door defined in the inspector.
+        */
+        manager.Track_Door_Order(door_Order, gameObject.tag);
+
+        if (isOpen == true)
+        {
+            // Sound Maybe ?
+            Debug.Log("OPENING!\n");
+        }
+
+        else
+        {
+            Debug.Log("CLOSING\n");
+            // Sound Maybe ?
+        }
+    }
+
+    void Start()
+    {
+        // Find reference to the GameManager object.
+        manager = FindObjectOfType<GameManager>();
+
+        // The door will begin closed.
+        isOpen = false;
+
+    }
+
+    void Update()
+    {
+        animator.SetBool("isOpen", isOpen);
+    }
+}
