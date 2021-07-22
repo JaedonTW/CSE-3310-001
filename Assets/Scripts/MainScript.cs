ded774a98fadc8bd193d9449e34cc94d871f0970
CJ13-projects <84744579+CJ13-projects@users.noreply.github.com>
2021-07-22 18:20:31 -0500

    dkfj

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+    public GameObject MainUI;
+   public GameObject LevelSelectUI;
+
+    public void Start()
+    {
+        MainUI.SetActive(true);
+        LevelSelectUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void EndGame()
+    {
+        Debug.Log("Done");
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        MainUI.SetActive(false);
+       // LevelSelectUI.SetActive(true);
+    }
+    public void Act1()
+    {
+        SceneManager.LoadScene("0");
+    }
+    public void Act2()
+    {
+        SceneManager.LoadScene("2");
+    }
+    public void Act3()
+    {
+        SceneManager.LoadScene("");
+    }
+}
+
