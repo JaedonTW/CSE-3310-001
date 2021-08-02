e466f06774960a95594765f9396201f5953961d0
arhands <andrewrhands@gmail.com>
2021-08-02 15:04:34 -0500

    Fixed GUI stuff

--- Assets/Scripts/Fundamental Scripts/EndLevelBox.cs
+++ Assets/Scripts/Fundamental Scripts/EndLevelBox.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndLevelBox : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class EndLevelBox : MonoBehaviour
         MainCharacter character = collision.GetComponent<MainCharacter>();
         if(character != null)
         {
-            print("Level end!");
+            character.OnLevelEnd();
+            SceneManager.LoadScene("Level2 - Library");
         }
     }
 }
