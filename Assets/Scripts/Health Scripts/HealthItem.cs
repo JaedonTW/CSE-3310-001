87bf5aa04deb02957dbed8245c57df211ba42eb8
arhands <andrewrhands@gmail.com>
2021-08-02 17:02:01 -0500

    fixed health spots

--- Assets/Scripts/Health Scripts/HealthItem.cs
+++ Assets/Scripts/Health Scripts/HealthItem.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class HealthItem : MonoBehaviour
 {
-    public int healthValue;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         MainCharacter mainCharacter = collision.GetComponentInParent<MainCharacter>();
         if (mainCharacter != null && mainCharacter.Health < 100)
         {
-            mainCharacter.Health = Mathf.Min(100, mainCharacter.Health + healthValue);
-            Destroy(gameObject);
+            mainCharacter.Health = 100;
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = Color.black;
         }
     }
 }
